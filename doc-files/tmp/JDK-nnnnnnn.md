4d0bb12e1aa9ca7719b348be1181323354b76c69
Kevin Rushforth <kcr@openjdk.org>
2025-02-21 14:03:40 -0800

    Add tmp dir with template files

---
+++ doc-files/tmp/JDK-nnnnnnn.md
@@ -0,0 +1,5 @@
+### TITLE (without the "Release Note: ")
+
+DESCRIPTION
+
+See [JDK-nnnnnnn](https://bugs.openjdk.org/browse/JDK-nnnnnnn) for more information.
---
+++ doc-files/tmp/create-md-files.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo 'JDK-8340003 JDK-8342622 Release Note: JavaFX 24 Requires JDK 22 or Later' > JDK-8340003.md
+echo 'JDK-8306707 JDK-8344142 Release Note: Pluggable Image Loading via javax.imageio' > JDK-8306707.md
+echo 'JDK-8345121 JDK-8346566 Release Note: JavaFX Warning Printed for Use of Terminally Deprecated Methods in sun.misc.Unsafe' > JDK-8345121.md
+echo 'JDK-8337280 JDK-8342623 Release Note: The `jdk.jsobject` Module is Now Included with JavaFX' > JDK-8337280.md
+echo 'JDK-8347744 JDK-8347745 Release Note: JavaFX Applications Must Use `--enable-native-access`' > JDK-8347744.md
+echo 'JDK-8341090 JDK-8342628 Release Note: JavaFX No Longer Supports Running With a Security Manager' > JDK-8341090.md
